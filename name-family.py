aaaf6077820ad0cfecde4b40984a20f53b3efc93
sgil <sgil@ualberta.ca>
2014-01-14 08:48:35 -0700

    updates

--- name-family.py
+++ name-family.py
@@ -3,7 +3,7 @@ class Student:
     name = ""
     def __init__(self, name, family):
         self.courseMarks = {}
-        self.name = name
+        self.name = name + " " + family
     
     def addCourseMarks(self, course, mark):
         self.courseMarks[course] = mark;
@@ -13,5 +13,12 @@ class Student:
             sum = 0
             for course, mark in self.courseMarks.iteritems():
                 sum += mark
-                return sum / len(self.courseMarks)
-        return 0
+            
+            print(self.name + " has an average of " + str(sum / len(self.courseMarks)))
+            return sum / len(self.courseMarks)
+        print(self.name + " has no courses.")
+    
+s1 = Student("John", "Baker")
+s1.addCourseMarks("c401", 44)
+s1.addCourseMarks("c307", 88)
+s1.average()
